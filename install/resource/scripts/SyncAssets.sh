5f420793d8c30e7e071354220082a12f4cac4c32
matt <mattythebatty@gmail.com>
2020-04-09 17:41:41 +0100

    add cache metadata when syncing assets

--- install/resource/scripts/SyncAssets.sh
+++ install/resource/scripts/SyncAssets.sh
@@ -5,11 +5,11 @@ BUCKET_NAME=
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #Admin
-aws s3 sync "${THIS_DIR}/../../upload/admin/view/image" "s3://${BUCKET_NAME}/assets/admin/view/image" --delete
-aws s3 sync "${THIS_DIR}/../../upload/admin/view/javascript" "s3://${BUCKET_NAME}/assets/admin/view/javascript" --delete
-aws s3 sync "${THIS_DIR}/../../upload/admin/view/stylesheet" "s3://${BUCKET_NAME}/assets/admin/view/stylesheet" --delete
+aws s3 sync "${THIS_DIR}/../../upload/admin/view/image" "s3://${BUCKET_NAME}/assets/admin/view/image" --delete --cache-control "public, max-age=31536000"
+aws s3 sync "${THIS_DIR}/../../upload/admin/view/javascript" "s3://${BUCKET_NAME}/assets/admin/view/javascript" --delete --cache-control "public, max-age=31536000"
+aws s3 sync "${THIS_DIR}/../../upload/admin/view/stylesheet" "s3://${BUCKET_NAME}/assets/admin/view/stylesheet" --delete --cache-control "public, max-age=31536000"
 
 #Catalog
-aws s3 sync "${THIS_DIR}/../../upload/catalog/view/javascript" "s3://${BUCKET_NAME}/assets/catalog/view/javascript" --delete
-aws s3 sync "${THIS_DIR}/../../upload/catalog/view/theme/default/image" "s3://${BUCKET_NAME}/assets/catalog/view/theme/default/image" --delete
-aws s3 sync "${THIS_DIR}/../../upload/catalog/view/theme/default/stylesheet" "s3://${BUCKET_NAME}/assets/catalog/view/theme/default/stylesheet" --delete
+aws s3 sync "${THIS_DIR}/../../upload/catalog/view/javascript" "s3://${BUCKET_NAME}/assets/catalog/view/javascript" --delete --cache-control "public, max-age=31536000"
+aws s3 sync "${THIS_DIR}/../../upload/catalog/view/theme/default/image" "s3://${BUCKET_NAME}/assets/catalog/view/theme/default/image" --delete --cache-control "public, max-age=31536000"
+aws s3 sync "${THIS_DIR}/../../upload/catalog/view/theme/default/stylesheet" "s3://${BUCKET_NAME}/assets/catalog/view/theme/default/stylesheet" --delete --cache-control "public, max-age=31536000"
